314ba0d4824b59edfa0a93b2767236438d6163e5
LorDClockaN <davor@losinj.com>
2016-02-05 18:44:50 +0100

    CMSDK: Themes Tile (2/2)
    Add logging.. don't know why we even do this LOL
    
    PS2: Added custom ROMCONTROL class
    
    Change-Id: Ic84721256e73e59b5fb2ecc9281f48fad3b387ff
    Signed-off-by: placiano <placiano80@gmail.com>

--- sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -60,6 +60,7 @@ public class QSConstants {
     public static final String TILE_DND = "dnd";
     public static final String TILE_NAVBAR = "navbar";
     public static final String TILE_FLOAT_MODE = "float_mode";
+    public static final String TILE_THEMES = "themes";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -111,6 +112,7 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_CAFFEINE);
         STATIC_TILES_AVAILABLE.add(TILE_NAVBAR);
         STATIC_TILES_AVAILABLE.add(TILE_FLOAT_MODE);
+        STATIC_TILES_AVAILABLE.add(TILE_THEMES);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
 
